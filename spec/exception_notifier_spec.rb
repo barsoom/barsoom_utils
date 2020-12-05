be4dd1ad30f1d74629ef833a9a0cbe4ee5c5195b
Olle Jonsson <olle.jonsson@auctionet.com>
2020-12-05 14:42:35 +0100

    Nest a spec

--- spec/exception_notifier_spec.rb
+++ spec/exception_notifier_spec.rb
@@ -1,65 +1,67 @@
 # frozen_string_literal: true
 require "barsoom_utils/exception_notifier"
 
-RSpec.describe BarsoomUtils::ExceptionNotifier, ".notify" do
-  before do
-    stub_const("Honeybadger", double)
+RSpec.describe BarsoomUtils::ExceptionNotifier do
+  describe ".notify" do
+    before do
+      stub_const("Honeybadger", double)
+    end
+
+    it "passes an exception to Honeybadger" do
+      ex = StandardError.new("boom")
+      expect(Honeybadger).to receive(:notify).with(ex)
+      BarsoomUtils::ExceptionNotifier.notify(ex)
+    end
+
+    it "can pass context info as well" do
+      ex = StandardError.new("boom")
+      expect(Honeybadger).to receive(:context).with(foo: "bar")
+      expect(Honeybadger).to receive(:notify).with(ex)
+      BarsoomUtils::ExceptionNotifier.notify(ex, context: { foo: "bar" })
+    end
+
+    it "complains if given a non-exception" do
+      expect(Honeybadger).not_to receive(:notify)
+
+      expect {
+        BarsoomUtils::ExceptionNotifier.notify({ foo: "bar" })
+      }.to raise_error(/Expected an exception but got:.*foo.*bar/)
+    end
   end
 
-  it "passes an exception to Honeybadger" do
-    ex = StandardError.new("boom")
-    expect(Honeybadger).to receive(:notify).with(ex)
-    BarsoomUtils::ExceptionNotifier.notify(ex)
-  end
+  describe ".message" do
+    before do
+      stub_const("Honeybadger", double)
+    end
 
-  it "can pass context info as well" do
-    ex = StandardError.new("boom")
-    expect(Honeybadger).to receive(:context).with(foo: "bar")
-    expect(Honeybadger).to receive(:notify).with(ex)
-    BarsoomUtils::ExceptionNotifier.notify(ex, context: { foo: "bar" })
-  end
+    it "passes a message to Honeybadger" do
+      expect(Honeybadger).to receive(:notify).with(
+        error_class: "Boom!",
+        error_message: "(no message)",
+        context: {},
+      )
+      BarsoomUtils::ExceptionNotifier.message("Boom!")
 
-  it "complains if given a non-exception" do
-    expect(Honeybadger).not_to receive(:notify)
+      expect(Honeybadger).to receive(:notify).with(
+        error_class: "Boom!",
+        error_message: "Details!",
+        context: {},
+      )
+      BarsoomUtils::ExceptionNotifier.message("Boom!", "Details!")
 
-    expect {
-      BarsoomUtils::ExceptionNotifier.notify({ foo: "bar" })
-    }.to raise_error(/Expected an exception but got:.*foo.*bar/)
-  end
-end
+      expect(Honeybadger).to receive(:notify).with(
+        error_class: "Boom!",
+        error_message: "Details!",
+        context: { foo: "bar" },
+      )
+      BarsoomUtils::ExceptionNotifier.message("Boom!", "Details!", foo: "bar")
 
-RSpec.describe BarsoomUtils::ExceptionNotifier, ".message" do
-  before do
-    stub_const("Honeybadger", double)
-  end
-
-  it "passes a message to Honeybadger" do
-    expect(Honeybadger).to receive(:notify).with(
-      error_class: "Boom!",
-      error_message: "(no message)",
-      context: {},
-    )
-    BarsoomUtils::ExceptionNotifier.message("Boom!")
-
-    expect(Honeybadger).to receive(:notify).with(
-      error_class: "Boom!",
-      error_message: "Details!",
-      context: {},
-    )
-    BarsoomUtils::ExceptionNotifier.message("Boom!", "Details!")
-
-    expect(Honeybadger).to receive(:notify).with(
-      error_class: "Boom!",
-      error_message: "Details!",
-      context: { foo: "bar" },
-    )
-    BarsoomUtils::ExceptionNotifier.message("Boom!", "Details!", foo: "bar")
-
-    expect(Honeybadger).to receive(:notify).with(
-      error_class: "Boom!",
-      error_message: "(no message)",
-      context: { foo: "bar" },
-    )
-    BarsoomUtils::ExceptionNotifier.message("Boom!", foo: "bar")
+      expect(Honeybadger).to receive(:notify).with(
+        error_class: "Boom!",
+        error_message: "(no message)",
+        context: { foo: "bar" },
+      )
+      BarsoomUtils::ExceptionNotifier.message("Boom!", foo: "bar")
+    end
   end
 end
